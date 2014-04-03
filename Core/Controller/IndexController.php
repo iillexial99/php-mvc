58c339b0ef998814a33f040e127c2da172977f1e
ivaaaan <ivan.klimenchenko@gmail.com>
2014-04-03 12:54:59 +0000

    IndexController.php edited

--- Core/Controller/IndexController.php
+++ Core/Controller/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends Controller{
          * print_r($this->config);
          * $model = new \Model\Index;
          * echo $model->test();
-         * $this->load->view('index)');
+         * $this->load->view('index');
          */
         
         
